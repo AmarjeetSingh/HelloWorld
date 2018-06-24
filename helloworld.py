47019cb08544d447fe0aa2269b25ca94a6b54df0
Amarjeet Singh <amarjeet.singh@tomtom.com>
2018-06-24 23:00:03 +0530

    hello world program added

---
+++ helloworld.py
@@ -0,0 +1,27 @@
+print("Hello world !")
+print(1+2)
+print()
+print(7*7)
+greeting = "hello"
+name = "Captain"
+print(greeting+" "+name)
+
+# name = input("Please enter your name: ")
+# print(greeting+" "+name)
+
+splitstring = "This string is splitted\nover several\nlines"    # next line
+print(splitstring)
+
+tabbedstring = "1\t2\t3\t4\t"                       # tabbed position
+print(tabbedstring)
+
+print('I think it\'s a good way to ask "what\'s up man ?"')        # \ will let ' take as a comment
+
+anotherspilt = """This is another split
+with diff
+lines"""
+print(anotherspilt)
+
+
+
+
